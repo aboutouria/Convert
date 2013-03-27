edc871b8722e320b7d371e086cdc380428cbc570
Anis Boutouria <aboutouria@gmail.com>
2013-03-27 19:36:24 -0400

    Test de commit

--- Convert/main.c
+++ Convert/main.c
@@ -8,11 +8,40 @@
 
 #include <stdio.h>
 
+const double VALEUR_CONVERSION = 0.62137;
+
+
+
+
+void cannot_altere(int n)
+{
+    n= 360;
+}
+void alter(int * const np){
+    *np = 120;
+}
+
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int n = 10;
+        int *const ad1 = &n;
+    
+    
+    printf("%d", *ad1);
+   
+    
+    
     return 0;
 }
+
+void echange(int * ad1, int * ad2){
+    
+    int x;
+    x = * ad1;
+    * ad1 = * ad2;
+    *ad2 = x;
+}
+
 
